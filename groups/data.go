8f585f03e1e09f69f384212a6724223f47c085c2
Daniel Waechtler <daniel.waechtler@student.uni-halle.de>
2022-02-14 15:45:42 +0100

    updating swagger

--- groups/data.go
+++ groups/data.go
@@ -19,7 +19,7 @@ import (
 // @Accept json
 // @Produce json
 // @Success 200 {string} message
-// @Router /data/{modelId} [get]
+// @Router /data/entity_extraction/{modelId} [get]
 func AddDataPoints(c *gin.Context) {
 	modelId := c.Param("modelId")
 
@@ -68,7 +68,7 @@ func AddDataPoints(c *gin.Context) {
 // @Accept json
 // @Produce json
 // @Success 200 {string} message
-// @Router /data/{modelId} [delete]
+// @Router /data/entity_extraction/{modelId} [delete]
 func DeleteDataPoints(c *gin.Context) {
 	modelId := c.Param("modelId")
 	var ids helper.IdBody
