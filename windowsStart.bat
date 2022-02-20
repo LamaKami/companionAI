4d59ad1bcf3ece55cbade42c42d8711948aa1a49
Daniel Waechtler <daniel.waechtler@student.uni-halle.de>
2022-02-20 18:48:01 +0100

    update start script and todos

--- windowsStart.bat
+++ windowsStart.bat
@@ -1,4 +1,4 @@
 FOR /f "tokens=*" %%i IN ('docker ps -q') DO docker kill %%i
-docker system prune
+docker system prune -y
 docker build -t companion_ai --build-arg value=%cd% .
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v %cd%:/companionAI/mnt -d -p 8080:8080 --rm companion_ai
